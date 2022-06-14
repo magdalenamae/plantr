d51806cf3672fc5eedb7c3241b75d211a9e41001
Eleanor Yeoell <eleanoryeoell@icloud.com>
2022-06-14 20:57:22 +1000

    added and updated various functions

--- client/js/components/displayPlantsList.js
+++ client/js/components/displayPlantsList.js
@@ -1,41 +1,36 @@
-
-
-
-function displayPlantsList(){
-    const section = document.getElementById('plants-details')
-    console.log("in display plants")
-    const loadingTag = document.createElement('p')
-    loadingTag.textContent = "loading"
-    const userid = 2
-    section.replaceChildren(loadingTag)
-    const heading = document.createElement("h1")
-    heading.classList.add('header-h1')
-    heading.textContent ="Green House"
-    axios
-        .get(`/api/plants/${userid}`)
-        .then((response)=>{
-            console.log(response.data)
-            const listElements = response.data.map((plant)=>displayPlant(plant,userid))
-            console.log(listElements)
-            section.replaceChildren(heading,...listElements)
-        })
+function displayPlantsList() {
+  const section = document.getElementById("plants-details");
+  console.log("in display plants");
+  const loadingTag = document.createElement("p");
+  loadingTag.textContent = "loading";
+  const userid = 2;
+  section.replaceChildren(loadingTag);
+  const heading = document.createElement("h1");
+  heading.classList.add("header-h1");
+  heading.textContent = "Green House";
+  axios.get(`/api/plants/${userid}`).then((response) => {
+    console.log(response.data);
+    const listElements = response.data.map((plant) => displayPlant(plant, userid));
+    console.log(listElements);
+    section.replaceChildren(heading, ...listElements);
+  });
 }
-function displayPlant(plant,userid){
-    const divEl = document.createElement('div')
-    console.log(divEl)
-    divEl.classList.add("plant")
-    divEl.setAttribute('id',plant.id)
-    const name = document.createElement('h2')
-    name.textContent = plant.name
-    const description = document.createElement('p') 
-    description.textContent = plant.description
-    const image = document.createElement("img")
-    image.src = plant.image
-    const linkViewMore = document.createElement('a')
-    linkViewMore.classList
-    linkViewMore.textContent ="View more"
-    linkViewMore.href = 'javascript:void(0)'
-    divEl.append(image,name,description,linkViewMore)
-    linkViewMore.addEventListener('click',showPlantsDetails)
-    return divEl
+function displayPlant(plant, userid) {
+  const divEl = document.createElement("div");
+  console.log(divEl);
+  divEl.classList.add("plant");
+  divEl.setAttribute("id", plant.id);
+  const name = document.createElement("h2");
+  name.textContent = plant.name;
+  const description = document.createElement("p");
+  description.textContent = plant.description;
+  const image = document.createElement("img");
+  image.src = plant.image;
+  const linkViewMore = document.createElement("a");
+  linkViewMore.classList;
+  linkViewMore.textContent = "See more";
+  linkViewMore.href = "javascript:void(0)";
+  divEl.append(image, name, description, linkViewMore);
+  linkViewMore.addEventListener("click", showPlantsDetails);
+  return divEl;
 }
--- client/js/components/header.js
+++ client/js/components/header.js
@@ -1,7 +1,19 @@
-console.log('hi')
-function renderHeader(){
-    
-    const buttonGetPlants = document.getElementById("get-plants")
-    console.log(buttonGetPlants)
-    buttonGetPlants.addEventListener('click',displayPlantsList)
+console.log("header running");
+function renderHeader() {
+  //view greenhouse
+  const buttonGetPlants = document.getElementById("get-plants");
+  console.log(buttonGetPlants);
+  buttonGetPlants.addEventListener("click", displayPlantsList);
+
+  //add plant
+  const addPlant = document.getElementById("add-plants");
+  console.log("added plants");
+  addPlant.addEventListener("click", (event) => {
+    console.log("success");
+    const addForm = document.getElementById("plants-details");
+    //form to be added in
+    addForm.textContent = "add your plant";
+  });
+
+  //logout function to be added
 }
