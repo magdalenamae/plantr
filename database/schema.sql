0c3a782b7bf0302e87f23aa224242cb2fc67cea1
Eleanor Yeoell <eleanoryeoell@icloud.com>
2022-06-14 21:38:01 +1000

    updated seed and schema files for DB

--- database/schema.sql
+++ database/schema.sql
@@ -1,24 +1,29 @@
 DROP TABLE if EXISTS care;
 DROP TABLE IF EXISTS plants;
 DROP Table if EXISTS users;
+DROP Table if EXISTS greenhouse;
 
 create table users (
-    id serial PRIMARY KEY ,
-
+    id serial PRIMARY KEY,
     name VARCHAR(255),
     email text UNIQUE,
-    password_hash VARCHAR(255)
-   
+    password_hash VARCHAR(255)  
 );
 
 create table plants (
     id serial PRIMARY KEY,
     name VARCHAR(255),
     image VARCHAR(255),
-    description VARCHAR(255),
-    user_id integer REFERENCES users(id)
-    
+    description VARCHAR(255) 
 );
+
+create table greenhouse (
+    id serial PRIMARY KEY,
+    name VARCHAR(255),
+    plant_id integer REFERENCES plants(id), 
+    user_id integer REFERENCES users(id) 
+);
+
 create table care (
     plant_id integer REFERENCES plants(id),
     care_id serial PRIMARY KEY,
