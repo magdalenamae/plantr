971d4aa79f0c31bca0073580331cb72b6b661832
magdalena <@gmail.com>
2022-06-15 15:11:13 +1000

    This is just an update fot all the plant images

--- client/js/components/displayPlantsList.js
+++ client/js/components/displayPlantsList.js
@@ -1,13 +1,17 @@
 function displayPlantsList() {
   const section = document.getElementById("plants-details");
   console.log("in display plants");
+  //
   const loadingTag = document.createElement("p");
   loadingTag.textContent = "loading";
+  //
   const userid = 2;
   section.replaceChildren(loadingTag);
+  //
   const heading = document.createElement("h1");
   heading.classList.add("header-h1");
   heading.textContent = "Green House";
+  //
   axios.get(`/api/plants/${userid}`).then((response) => {
     console.log(response.data);
     const listElements = response.data.map((plant) => displayPlant(plant, userid));
@@ -15,22 +19,40 @@ function displayPlantsList() {
     section.replaceChildren(heading, ...listElements);
   });
 }
+
 function displayPlant(plant, userid) {
   const divEl = document.createElement("div");
   console.log(divEl);
   divEl.classList.add("plant");
   divEl.setAttribute("id", plant.id);
+  // 
   const name = document.createElement("h2");
   name.textContent = plant.name;
+  //
   const description = document.createElement("p");
   description.textContent = plant.description;
+  //
   const image = document.createElement("img");
   image.src = plant.image;
+  //
   const linkViewMore = document.createElement("a");
   linkViewMore.classList;
   linkViewMore.textContent = "See more";
   linkViewMore.href = "javascript:void(0)";
+  //
   divEl.append(image, name, description, linkViewMore);
   linkViewMore.addEventListener("click", showPlantsDetails);
   return divEl;
 }
+
+ function showPlantCareDetails(){
+  let showCareDetails = document.createElement("linkViewMore");
+  console.log('hello world, this is the showPlantCareDetails function');
+  showCareDetails.addEventListener("click", (event) => {
+    // axios event to get care details that match the plant id 
+    // render the care details with the plant name and description and image 
+    //add catch details to the axios event
+  });
+ }
+
+ plantcareDetails()
--- database/seed.sql
+++ database/seed.sql
@@ -1,21 +1,22 @@
--- USERS
-INSERT INTO users (name, email, password_hash) VALUES ('sally', 'sally@example.com', 'sally');
-INSERT INTO users (name, email, password_hash) VALUES ('sam', 'sam@example.com', 'sam');
 
--- PLANTS
-INSERT INTO plants (name, image, description) VALUES ('Monstera deliciosa', 'https://images.pexels.com/photos/2090641/pexels-photo-2090641.jpeg', 'an indoor plant needing a lot of space and jailing from parts of southern Mexico.');
-INSERT INTO plants (name, image, description) VALUES ('Hoya Kerrii', 'https://media.istockphoto.com/photos/colorful-flowers-sweetheart-hoya-leaf-pot-ornamental-plant-or-hoya-picture-id1127518168?b=1&k=20&m=1127518168&s=612x612&w=0&h=G3yV3LrOCud09d22b9uArbLKMW4r0EC_HeyzFsN5M74=', 'a heart shapped leaf plant from southeast Asia');
-INSERT INTO plants(name, image, description) VALUES ('Hoya linearis', 'https://images.squarespace-cdn.com/content/v1/5bdc80984cde7aec2e94e22d/1610366612109-DKACGR4Z9OK5HM1X3PPK/IMG_7547.jpg?format=2500w', 'Distinctly unique with delicate foliage that is elongated, soft and slightly hairy.');
-INSERT INTO plants(name, image, description) VALUES ('Begonia mazae', 'https://quickbutik.imgix.net/14829R/products/62430256acb73.jpeg?auto=format', 'tear-shaped dark green leaves overlaid with a blackish pattern');
-INSERT INTO plants(name, image, description) VALUES ('Aglaonema stripes', 'https://i.etsystatic.com/10494852/r/il/88a04a/3093502645/il_300x300.3093502645_2v0r.jpg', 'Beautiful and easy to care for, with dark green leaves and white stripes.');
 
--- GREENHOUSE
-INSERT INTO greenhouse (name, plant_id, user_id) VALUES ('Allan', 5, 2);
+INSERT into users (name,email,password_hash) values ('sally','sally@example.com','sally');
+INSERT into users (name,email,password_hash) values ('sam','sam@example.com','sam');
+-- 
+
+INSERT INTO plants (name,image,description,user_id) values ('Monstera deliciosa','https://images.pexels.com/photos/2090641/pexels-photo-2090641.jpeg','an indoor plant needing a lot of space and jailing from parts of southern Mexico.',1);
+INSERT INTO plants (name,image,description,user_id) values ('Hoya Kerrii','https://media.istockphoto.com/photos/colorful-flowers-sweetheart-hoya-leaf-pot-ornamental-plant-or-hoya-picture-id1127518168?b=1&k=20&m=1127518168&s=612x612&w=0&h=G3yV3LrOCud09d22b9uArbLKMW4r0EC_HeyzFsN5M74=','a heart shapped leaf plant from southeast Asia',2);
+INSERT INTO plants(name,image,description,user_id) values ('Hoya linearis','https://images.squarespace-cdn.com/content/v1/5bdc80984cde7aec2e94e22d/1610366612109-DKACGR4Z9OK5HM1X3PPK/IMG_7547.jpg?format=2500w','Distinctly unique with delicate foliage that is elongated, soft and slightly hairy.',2);
+INSERT INTO plants(name,image,description,user_id) values ('Begonia mazae','https://quickbutik.imgix.net/14829R/products/62430256acb73.jpeg?auto=format','tear-shaped dark green leaves overlaid with a blackish pattern',1);
+INSERT INTO plants(name,image,description,user_id) values ('Aglaonema ''stripes','https://i.etsystatic.com/10494852/r/il/88a04a/3093502645/il_300x300.3093502645_2v0r.jpg','Beautiful and easy to care for, with dark green leaves and white stripes.',2);
+
+
+
 
 -- CARE: water:low-moderate, humidity : high , Light: bright indirect, soil: well-draining
-INSERT INTO care (plant_id, water, humidity, soil, light) VALUES (1, 'moderate','medium','well-draining', 'bright indirect');
-INSERT INTO care (plant_id, water, humidity, soil, light) VALUES (2, 'moderate','high','well-draining', 'bright indirect');
-INSERT INTO care (plant_id, water, humidity, soil, light) VALUES (2, 'moderate', 'low', 'well-draining', 'bright indirect' );
-INSERT INTO care (plant_id, water, humidity, soil, light) VALUES (1, 'moderate', 'medium-high', 'well-draining', 'bright indirect' );
-INSERT INTO care (plant_id, water, humidity, soil, light) VALUES (1, 'moderate', 'medium', 'well-draining', 'low-moderate' );
+INSERT INTO care(plant_id,water,humidity,soil,light) values (1,'moderate','medium','well-draining','bright indirect');
+INSERT INTO care(plant_id,water,humidity,soil,light) values (2,'moderate','high','well-draining','bright indirect');
+INSERT INTO care(plant_id,water,humidity,soil,light) values (2,'moderate','low','well-draining', 'bright indirect' );
+INSERT INTO care(plant_id,water,humidity,soil,light) values (1,'moderate','medium-high','well-draining', 'bright indirect' );
+INSERT INTO care(plant_id,water,humidity,soil,light) values (1,'moderate','medium','well-draining', 'low-moderate' );
 
