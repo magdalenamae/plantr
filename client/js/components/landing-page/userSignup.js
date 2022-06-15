fcbe7d9f78194ba0ca369940df483ba9e55934b3
Eleanor Yeoell <eleanoryeoell@icloud.com>
2022-06-15 23:53:51 +1000

    added server side js for signup page

--- client/js/components/landing-page/userSignup.js
+++ client/js/components/landing-page/userSignup.js
@@ -1,3 +1,4 @@
+console.log("signup running");
 // fucntion to create the Sign Up form
 function renderSignUpForm() {
   // getting body element from the html page
@@ -9,35 +10,39 @@ function renderSignUpForm() {
   form.innerHTML = `
     <input type="text" name="name" placeholder="name" />
     <input type="text" name="email" placeholder="email" autocomplete="off" />
-    <input type="password" name="password_hash" placeholder="password" autocomplete="off" />
+    <input type="password" name="password" placeholder="password" autocomplete="off" />
     <button>Sign Up</button>
     `;
   main.replaceChildren(heading, form);
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    // getting form data to
     newFormData = new FormData(form);
+
     const data = {
       name: newFormData.get("name"),
       email: newFormData.get("email"),
       password: newFormData.get("password"),
     };
+    console.log(data);
+
     // sending form data to server/ database
-    axios
-      .post("/plantr/users", data)
-      .then((response) => {
-        console.log("user saved", response);
-        // render green house page to be instered here
-      })
-      .catch((error) => {
-        console.error(error.status);
-        if (error.response.status === 400) {
-          const reason = error.response.data.message;
-          alert(reason);
-        } else {
-          alert("something went wrong");
-        }
-      });
+
+    axios.post("/api/users", data).then(() => (window.location = "/greenhouse.html"));
   });
 }
+// axios.post("/api/users", data).then((response) => {
+//   console.log("user saved", response);
+// });
+//deleted the below because we have this covered in server.js
+// .catch((error) => {
+//   console.error(error.status);
+//   if (error.response.status === 400) {
+//     const reason = error.response.data.message;
+//     alert(reason);
+//   } else {
+//     alert("something went wrong");
+//   }
+// });
+// });
+// }
--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-console.log('hi')
+console.log("hi");
 const express = require("express");
-const db = require('./database/db.js')
+const db = require("./database/db.js");
 
-//const pgSession = require('connect-pg-simple')(expressSession);
 const expressSession = require("express-session");
 const pgSession = require("connect-pg-simple")(expressSession);
 
-
-//const userController = require('./controllers/users.js')
-const plantsControllers = require('./controllers/plants.js')
+//controllers
+const plantsControllers = require("./controllers/plants.js"); //swap with greenhouse in PR
 const sessionController = require("./controllers/session");
-const app = express()
+const signupController = require("./controllers/signup");
+
+const app = express();
 const port = 3000;
-app.use(express.static('client'))
-app.use(express.json())
-
-
+app.use(express.static("client"));
+app.use(express.json());
 
 //code for create session
 app.use(
@@ -28,28 +26,28 @@ app.use(
   })
 );
 
-//
-//middleware 
+//middleware
 app.use((req, res, next) => {
-    console.log(`${new Date()} ${req.method} ${req.path}`);
-    next()
-  })
+  console.log(`${new Date()} ${req.method} ${req.path}`);
+  next();
+});
 
 //plants API
-app.use('/api/plants',plantsControllers)
+app.use("/api/plants", plantsControllers); //swap with greenhouse in PR
 app.use("/api/session", sessionController);
-//handle any error that wasn't handled
+app.use("/api/users", signupController);
 
-app.use((err,req,res,next)=>{
-    let status = err.status || 500;
+//handle any error that wasn't handled
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
   let message = err.message || "something went wrong";
 
   res.status(status).json({ message });
 
   next(err);
-})
+});
 
 // start the web server
 app.listen(port, () => {
-    console.log(`listening on port http://localhost:${port}`);
-  });
+  console.log(`listening on port http://localhost:${port}`);
+});
