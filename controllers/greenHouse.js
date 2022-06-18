c611d47728b581da64e1a2d43b2db62df13fd473
Eleanor Yeoell <eleanoryeoell@icloud.com>
2022-06-18 11:00:43 +1000

    uploaded ankitas greenHouse.js file

--- controllers/greenHouse.js
+++ controllers/greenHouse.js
@@ -1,8 +1,10 @@
-const { response } = require("express");
-const express = require("express");
 
-const router = express.Router();
-const db = require("../database/db.js");
+const { response } = require('express');
+const express = require('express');
+const format = require('pg-format');
+const router = express.Router()
+const db = require('../database/db.js')
+
 
 // get all data
 router.get("/", (req, res) => {
@@ -30,14 +32,71 @@ router.get("/session/:id", (req, res) => {
         return;
       }
     })
-    .catch((error) => {
-      console.log("error", error);
-      res.status(400).json({ success: false, message: error });
-    });
-});
-router.post("/", (req, res) => {
-  const reqDtata = req.body;
-  const sql = "INSERT into greenhouse";
+
+})
+router.post('/',(req,res)=>{
+    const reqData= req.body
+    const username = reqData['name']
+    const userid = reqData['userid']
+    const plantidsArrayString = reqData['plantid']
+    const plantsidNumArray = plantidsArrayString.map((a)=>parseInt(a))
+
+    // const sql ='INSERT into greenhouse(name,plant_id,user_id) values ($1,$2,$3)'
+    // db.query(sql,[reqDtata['name'],reqDtata['plantid'],reqDtata['userid']])
+    console.log(reqData,"reqData")
+    console.log(username,"username")
+    console.log(userid,"userid")
+    console.log(plantsidNumArray,"plants.id")
+
+    let values = [
+        // ['sam1', 3, 2],
+        // ['sam1',3,3]
+    ];
+
+    plantsidNumArray.forEach(value => {
+       
+       values.push(dataArray(username,userid,value))
+       
+    })
+    console.log(values,"ee hai value")
+    
+    //lets try it this way
+
+    return db.query(format('INSERT INTO greenhouse (name, user_id, plant_id) VALUES %L', values))
+    .then((result) => {
+        console.log("this ")
+        console.log(result)
+        res.json({ success: true })
+      })
+      .catch((err)=>{
+
+
+        res.status(500).json({success:false,message:'error'})
+      })
+  });
+    
+    
+    
+    // , (err, result)=>{
+    //     res.status(200).json({ success:true });
+    //   })
+    //   .catch((error)=>{
+    //     console.log('error',error)
+    //     res.status(400).json({success:false, message: error})
+    // })
+
+  
+
+// })
+
+function dataArray(username,userid,plantid){
+    console.log(username,userid,plantid)
+    
+    return [username, userid, plantid];
+        
+}
+
+
 
   console.log(reqDtata, "reqDtata");
 });
