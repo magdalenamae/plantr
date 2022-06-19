13e4417f9bd4614de6f172bf6c60d54e9b9650b6
Ankita Sawrav <ankita.sawrav@gmail.com>
2022-06-19 12:09:49 +1000

    fix for displaying logged in user plants

--- client/js/components/displayPlantsList.js
+++ client/js/components/displayPlantsList.js
@@ -17,39 +17,64 @@ function displayPlantsList() {
   
 
 
-//new approach trying 
+//
 
 //
-const userid =23
+// const userid =23
 //api session
-axios.get(`/api/session`)
-  .then((response)=>{
-  console.log("in session")
-    console.log(response.data.id,"session")
-     let userid = response.data.id
-     console.log(userid)
-    //  useridfromsession = userid
-    })
-    // console.log(useridfromsession)
+// axios.get(`/api/session`)
+//   .then((response)=>{
+//   console.log("in session")
+//     console.log(response.data.id,"session")
+//      let userid = response.data.id
+//      console.log(userid)
+//     //  useridfromsession = userid
+//     })
+//     // console.log(useridfromsession)
 
-// console.log(sessionUserid,"sessionUserid,")
-  axios.get(`/api/greenHouse/${userid}`)
-  .then((response) => {
-    console.log(response.data);
+// // console.log(sessionUserid,"sessionUserid,")
+//   axios.get(`/api/greenHouse/${userid}`)
+//   .then((response) => {
+//     console.log(response.data);
     
-    const listElements = response.data.map((plant) => displayPlant(plant, userid));
+//     const listElements = response.data.map((plant) => displayPlant(plant, userid));
+//     console.log(listElements);
+//     section.replaceChildren(heading, ...listElements);
+//   })
+//   .catch((error)=>{
+//     console.log(error.response)
+//     const errormsg = document.createElement('p')
+//     errormsg.textContent = "You dont have any plants to display. Please add some plants."
+//     section.replaceChildren(errormsg)
+//   })
+ 
+// }
+
+//********************* CODE FOr async wait ti get data grom green house for the logged in user*/
+
+sessionurl = 'http://localhost:3000/api/session'
+const getUserid = async(sessionurl)=>{
+  try {
+    const response =await axios(sessionurl);
+    console.log('response.data',response.data)
+    greenhouseurl =`/api/greenHouse/${response.data.id}`
+    const response2 = await axios(greenhouseurl)
+    console.log('response2 data',response2)
+    const listElements = response2.data.map((plant) => displayPlant(plant, response.data.id));
     console.log(listElements);
     section.replaceChildren(heading, ...listElements);
-  })
-  .catch((error)=>{
+  }catch(error){
+    console.log(error)
     console.log(error.response)
     const errormsg = document.createElement('p')
     errormsg.textContent = "You dont have any plants to display. Please add some plants."
     section.replaceChildren(errormsg)
-  })
- 
+  }
 }
 
+getUserid(sessionurl)
+}
+//********************* */
 function displayPlant(plant, userid) {
   const divEl = document.createElement("div");
   console.log(divEl);
