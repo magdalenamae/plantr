b75c310c5053abcfab9a57b5918ed6c14b9cd31a
magdalena <@gmail.com>
2022-06-19 15:20:03 +1000

    This is the show care details

--- client/js/components/plantsddetails.js
+++ client/js/components/plantsddetails.js
@@ -1,8 +1,52 @@
 function showPlantsDetails(){
-
+    const page = document.getElementById('plants-details')
+    page.innerHTML = ''
     const id =  this.parentElement.id
     console.log("id",id)
+    
 
+    axios.get(`/api/care/${id}`)
+    .then((response) => {
+       console.log(response.data)
+       
+       for (result of response.data){
+        careId = result.id
+        plantName = result.name
+        plantDescription = result.description
+        plantImage = result.image
+        water = result.water
+        light = result.light
+        humidity = result.humidity
+        soil = result.soil
+
+        console.log(id,plantName)
+        
+       }
+       const careDiv = document.createElement('div')
+       careDiv.classList.add('care-div')
+       const head = document.createElement('h1')
+       head.textContent = plantName
+      
+       const careDesc = document.createElement('p')
+       careDesc.textContent = plantName + ' needs the following care to thrive and be its best self'
+       const img = document.createElement('img')
+       img.src = plantImage
+       const careList = document.createElement('ul')
+       careList.classList.add('care-list')
+         const waterLi = document.createElement('li')
+             waterLi.textContent =  water + ' water levels'
+         const lightLi = document.createElement('li')
+            lightLi.textContent = light + ' light exposure'
+         const humidityLi = document.createElement('li')
+            humidityLi.textContent = humidity + ' humidity levels'
+        const soilLi = document.createElement('li')
+            soilLi.textContent = soil + ' soil'
+       
+       careList.append(waterLi,lightLi,humidityLi,soilLi) 
+       page.appendChild(careDiv)
+       careDiv.append(head,img,careDesc, careList)
+    })
+   
     // axios
     // .get(`/api/plants/${plant}`)
     // .then((response)=>{
@@ -12,3 +56,5 @@ function showPlantsDetails(){
     //     section.replaceChildren(heading,...listElements)
     // })
 }
+
+showPlantsDetails()
